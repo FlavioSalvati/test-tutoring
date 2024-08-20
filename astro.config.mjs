057c198b2229c188192ce18c9de96a51dc9e29ae
Flavio <fs483@cam.ac.uk>
2024-08-20 14:31:27 +0200

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import { writeFileSync } from 'fs';
-import { resolve, dirname } from 'path';
-import { fileURLToPath } from 'url';
 //test
 export default defineConfig({
   site: 'https://test.beyond-tutors.com',
-  build: {
-    assetsDir: 'assets', // Move assets to the assets directory
-  },
   integrations: [
     mdx(), // Enables MDX support
   ],
